120dbe79259495ba521f758a1da69834df0ee6fb
Cristichi <cristichi@hotmail.es>
2022-09-18 15:43:43 +0200

    First commit

---
+++ Card Photocopier/src/es/cristichi/cardphotocopier/obj/CardInfo.java
@@ -0,0 +1,12 @@
+package es.cristichi.cardphotocopier.obj;
+
+public class CardInfo {
+	public int copies, deck;
+	
+	public CardInfo(int copies, int deck) {
+		this.copies = copies;
+		this.deck = deck;
+	}
+	
+	
+}
