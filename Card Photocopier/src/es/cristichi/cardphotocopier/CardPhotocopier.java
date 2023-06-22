134391412c04c90753aa35c5669ef8ef5a2fd969
Cristichi <cristichi@hotmail.es>
2023-06-22 17:30:40 +0200

    Version 2.4.4

--- Card Photocopier/src/es/cristichi/cardphotocopier/CardPhotocopier.java
+++ Card Photocopier/src/es/cristichi/cardphotocopier/CardPhotocopier.java
@@ -53,7 +53,7 @@ import es.cristichi.cardphotocopier.obj.Range;
  * @author Cristichi#5193
  */
 public class CardPhotocopier {
-	private static String VERSION = "v2.4.3";
+	private static String VERSION = "v2.4.4";
 	private static String NAME = "Villainous Card Photocopier " + VERSION;
 
 	private static String CONFIG_TXT = "config.yml";
@@ -606,7 +606,7 @@ public class CardPhotocopier {
 					for (int i = 0; i < jsonExtras.length; i++) {
 						jsonExtras[i].put("cards", cardsExtras[i]);
 						jsonExtras[i].put("count", countExtras[i]);
-						jsonT.put(extraDecks[i], jsonExtras[i]);
+						jsonT.put(extraDecks[i].toLowerCase(), jsonExtras[i]);
 					}
 
 					resultsFolder.mkdirs();
